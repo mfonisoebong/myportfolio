72ff8f07f4c97bc56c3b83f1465f78862171e3f0
mfoniso <mfonisoischris@gmail.com>
2024-10-27 23:33:46 +0100

    features: changed error and not-found pages layouts

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Error({
@@ -9,13 +11,39 @@ export default function Error({
   error: Error;
   reset: () => void;
 }) {
+  const { theme } = useTheme();
+
   useEffect(() => {
     console.error(error);
   }, [error]);
 
+  const image =
+    theme === "dark" ? "/bug-fixing-dark.svg" : "/bug-fixing-light.svg";
+
   return (
-    <div>
-      <p>Oops! Something went wrong... maybe try refreshing?</p>
+    <div className="flex flex-col gap-8 items-center">
+      <Image
+        src={image}
+        className="object-cover w-8/12 md:w-72"
+        alt="Error"
+        width={400}
+        height={400}
+      />
+      <div className="space-y-3 text-center">
+        <h1 className="text-2xl lg:text-3xl font-semibold">
+          An error occcured
+        </h1>
+        <p className="text-sm md:text-base">
+          Oops! Something went wrong... maybe try refreshing? If you're the
+          developer you might want to check the browser console for the error
+        </p>
+        <button
+          onClick={reset}
+          className="md:text-lg border-b-2 hover:border-black active:opacity-50 duration-200"
+        >
+          Click here to reset
+        </button>
+      </div>
     </div>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import type { Metadata } from "next";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "404",
@@ -6,14 +11,34 @@ export const metadata: Metadata = {
 };
 
 export default function NotFound() {
+  const { theme } = useTheme();
+
+  const image = `/404-${theme}.svg`;
+
   return (
-    <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tight">
-        404 - Page not found
-      </h1>
-      <p className="mb-4">
-        Oops! The page you're looking for doesn't seem to exist.
-      </p>
-    </section>
+    <div className="flex flex-col gap-8 items-center">
+      <Image
+        src={image}
+        className="object-cover w-8/12 md:w-72"
+        alt="Error"
+        width={400}
+        height={400}
+      />
+      <div className="space-y-3 text-center">
+        <h1 className="text-2xl lg:text-3xl font-semibold">
+          404 - Page not found
+        </h1>
+        <p className="text-sm md:text-base">
+          Oops! Looks like you're in the wrong page or the link for this page is
+          broken
+        </p>
+        <Link
+          href={"/"}
+          className="md:text-lg border-b-2 hover:border-black active:opacity-50 duration-200"
+        >
+          Go to home
+        </Link>
+      </div>
+    </div>
   );
 }
