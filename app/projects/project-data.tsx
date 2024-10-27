9278481758caf363c7ad14106b72f761f07fdc88
mfoniso <mfonisoischris@gmail.com>
2024-10-27 20:50:48 +0100

    chore: added a project to project list

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -10,37 +10,49 @@ export const projects: Project[] = [
     title: "MBA",
     url: "https://mbathebrand.com",
     year: 2024,
-    description: "MBA is a brand that is focused on providing quality suites and suites accessories for MBA customers"
+    description:
+      "MBA is a brand that is focused on providing quality suites and suites accessories for MBA customers",
   },
   {
     title: "Spruce",
-    url: 'https://sprucecty.com',
+    url: "https://sprucecty.com",
     year: 2024,
-    description: "Explore our new arrivals and elevate your style to the next level! With our collection of unique styles and designs, you can be sure to find the perfect piece for any occasion."
+    description:
+      "Explore our new arrivals and elevate your style to the next level! With our collection of unique styles and designs, you can be sure to find the perfect piece for any occasion.",
   },
   {
     title: "Regain Pharmacy",
-    url: 'https://regainpharmacy.com',
+    url: "https://regainpharmacy.com",
     year: 2024,
-    description: "Your trusted destination for pharmaceutical needs, we offer you the support when you need it the most, plus discreet doorstep delivery."
+    description:
+      "Your trusted destination for pharmaceutical needs, we offer you the support when you need it the most, plus discreet doorstep delivery.",
+  },
+  {
+    title: "DFET",
+    url: "https://defutureelite.com",
+    year: 2024,
+    description:
+      "DFET is a team of professional software engineers with several years of experience in the delivery of superior software products",
   },
   {
     title: "Chowopa",
     url: "https://chowopa.com",
     year: 2023,
-    description: "Your trusted food delivery service, On the Go. The meals you love delivered with care wherever you are."
+    description:
+      "Your trusted food delivery service, On the Go. The meals you love delivered with care wherever you are.",
   },
   {
     title: "TBM Events",
     url: "https://thetbmevents.com",
     year: 2023,
-    description: "Discover Unforgettable Experiences: Your Gateway to Exclusive Events and Seamless Ticketing!"
+    description:
+      "Discover Unforgettable Experiences: Your Gateway to Exclusive Events and Seamless Ticketing!",
   },
   {
     title: "Ployco Music",
     url: "https://ployco.com",
     year: 2022,
-    description: "We provide promotional services, such as playlist placement and marketing campaigns, to help get your music in front of more listeners"
+    description:
+      "We provide promotional services, such as playlist placement and marketing campaigns, to help get your music in front of more listeners",
   },
-
 ];
